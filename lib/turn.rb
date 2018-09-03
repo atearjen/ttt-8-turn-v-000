896f94d683bfb9ef2456173da482642fb796a6c2
Allison Tearjen <amtearjen@ucdavis.edu>
2018-09-03 01:14:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,10 +3,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} " 
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?
+def valid_move?(board,index)
 
 end
 
